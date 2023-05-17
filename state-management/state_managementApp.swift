4d000a47db842ec0ff7b3c1f832e2ac4c04389c3
Aeshma <madhushanka2727@gmail.com>
2023-05-17 15:46:26 +0530

    Initial Commit

---
+++ state-management/state_managementApp.swift
@@ -0,0 +1,17 @@
+//
+//  state_managementApp.swift
+//  state-management
+//
+//  Created by Admin on 2023-05-17.
+//
+
+import SwiftUI
+
+@main
+struct state_managementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
