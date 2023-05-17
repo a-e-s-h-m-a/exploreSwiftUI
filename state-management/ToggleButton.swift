f9a00cc582530447ab966463ab5a081a93426c21
Aeshma <madhushanka2727@gmail.com>
2023-05-17 21:36:11 +0530

    implement toggle button

---
+++ state-management/ToggleButton.swift
@@ -0,0 +1,46 @@
+//
+//  ToggleButton.swift
+//  state-management
+//
+//  Created by Admin on 2023-05-17.
+//
+
+import SwiftUI
+
+struct ToggleButton: View {
+    
+    @Binding var state: Bool
+    
+    var body: some View {
+        Button {
+            withAnimation {
+                state.toggle()
+            }
+        } label: {
+            GeometryReader { proxy in
+                ZStack {
+                    Circle()
+                        .stroke(lineWidth: 2.0)
+                        .foregroundColor(.blue)
+                    
+                    if state {
+                        Circle()
+                            .frame(width: proxy.size.width * 0.8)
+                            .foregroundColor(.blue)
+                    }
+                }
+                .padding(8)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct ToggleButton_Previews: PreviewProvider {
+    static var previews: some View {
+        StatefulPreviewWrapper(true) { state in
+            ToggleButton(state: state)
+                .previewLayout(.fixed(width: 100, height: 100))
+        }
+    }
+}
